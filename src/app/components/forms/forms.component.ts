8b70e34ab0a14f361b6e037a6ea0f665dfaf7fb2
CaioSouzaDevv <caioss20.cs@gmail.com>
2024-02-07 06:40:29 -0300

    [FEAT] add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
-  results = [];
+export class FormsComponent implements OnInit {
+  @Output() submitEvent = new EventEmitter();
+  results: Location[] = [];
+  filteredResults: Location[] = [];
+  formGroup!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+   
+  }
+
+  onSubmit(): void {
+console.log(this.formGroup.value);  
+
+    this.submitEvent.emit();
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
+
 }
